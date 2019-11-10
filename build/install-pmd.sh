fcc22bc5d0ae86e307e3a49dc832f64bf4153c31
Aravindh <42031270+Aravindh04@users.noreply.github.com>
2019-11-10 20:21:05 +1100

    updates

---
+++ build/install-pmd.sh
@@ -0,0 +1,6 @@
+
+#Install PMD
+echo "Install PMD"
+curl -L "https://github.com/pmd/pmd/releases/download/pmd_releases/6.0.1/pmd-bin-6.0.1.zip" -o pmd-bin-6.0.1.zip
+unzip pmd-bin-6.0.1.zip
+rm pmd-bin-6.0.1.zip
